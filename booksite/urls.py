03d40ccba4f87056f1dc0f23955220be0a6574fa
Владимир <vladimirbright@gmail.com>
2010-10-11 23:18:18 +0400

    some templates

--- booksite/urls.py
+++ booksite/urls.py
@@ -6,9 +6,15 @@ from django.contrib import admin
 
 admin.autodiscover()
 
+direct = 'django.views.generic.simple.direct_to_template'
+
 urlpatterns = patterns('',
-    url( r'^$', 'django.views.generic.simple.direct_to_template',
-                                                { 'template': 'index.html'}),
+    url( r'^$', direct, { 'template': 'index.html'}, name='index'),
+    url( r'^authors/$', direct, { 'template': 'authors.html'}, name='authors'),
+    url( r'^books/$', direct, { 'template': 'books.html'}, name='books'),
+    url( r'^genres/$', direct, { 'template': 'genres.html'}, name='genres'),
+    url( r'^reading/$', direct, { 'template': 'reading.html'}, name='reading'),
+    url( r'^my-library/$', direct, { 'template': 'my.html'}, name='my'),
 
     (r'^admin/', include(admin.site.urls)),
 )
